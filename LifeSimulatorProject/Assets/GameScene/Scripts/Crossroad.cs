a37e588d6308b86141a4598812c69f94f1184c9a
loryconti <loryconti1@gmail.com>
2023-09-04 16:38:15 +0200

    Removed duplicate assets folder - Version 1.0.0

--- LifeSimulatorProject/Assets/GameScene/Scripts/Crossroad.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Crossroad : MonoBehaviour
-{
-    [Range(1f, 10f)] public float speedReductionFactor = 2f;
-}
